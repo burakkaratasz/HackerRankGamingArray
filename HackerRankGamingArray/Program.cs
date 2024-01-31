64157320f316e8055604072bd8948539bc258f0f
burakkaratasz <burakkaratasz@outlook.com>
2024-01-31 14:53:18 +0300

    solution

---
+++ HackerRankGamingArray/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'gamingArray' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static string gamingArray(List<int> arr)
+    {
+        
+        int count = 0;
+        int pre_num = 0;
+        arr.ForEach(delegate (int num)
+        {
+            if (num > pre_num) //mevcut eleman gecilenden buyuk mu
+            {
+                count++;
+                pre_num = num;
+            };
+        });
+        return count%2 == 0?"ANDY":"BOB"; //oyun 2 nin katlarinda bittiyse andy
+        
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = Result.gamingArray(arr);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
